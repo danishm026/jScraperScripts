4b5641966041b8d4b949f2338b33cd82d3d17ffe
Mohd Danish <danishm026@gmail.com>
2017-06-24 17:22:28 +0530

    Temp

--- scrape.py
+++ scrape.py
@@ -1,24 +1,67 @@
-#!/usr/bin/python3
+#!/usr/bin/python
+from optparse import OptionParser
 
 import os
 import json
 
-OUTPUT_DIRECTORY = os.environ['SCRAPER_OUTPUT_DIRECTORY']
-SCRAPE_COMMAND = 'java -Doutput.directory=' + OUTPUT_DIRECTORY + ' -cp uber-jScraper-0.0.1-SNAPSHOT.jar com.arc.jScraper.main.Main '
-DOWNLOAD_COMMAND = 'wget -U="" --continue '
+import subprocess
+
+
+OUTPUT_DIRECTORY = "/home/danish/.scraper/output"
 MODEL_PAGES = 'modelPages'
 IMAGE_DATA_LIST = 'imageDataList'
 IMAGE_URL = 'imageUrl'
 
 
+def scrape_model_command(model_name):
+    scrape_command = ['java']
+    scrape_command.append('-cp')
+    scrape_command.append('uber-jScraper-0.0.1-SNAPSHOT.jar')
+    scrape_command.append('com.arc.jScraper.main.Main')
+    scrape_command.append(model_name)
+    subprocess.call(scrape_command)
+
+
+def download_image(url, download_directory):
+    download_command = ['wget']
+    download_command.append('-U')
+    download_command.append('')
+    download_command.append('--continue')
+    download_command.append('--directory-prefix')
+    download_command.append(download_directory)
+    download_command.append(url)
+    subprocess.call(download_command)
+
+
+def replace_space_with_underscore(string):
+    return "_".join(string.strip().split())
+
+
+def parse_command_line_arguments():
+    parser = OptionParser()
+    parser.add_option("-s", "--scrape", dest="scrape", action="store_true",
+                      help="Specify whether to scrape model or not")
+    parser.add_option("-d", "--download", dest="download", action="store_true",
+                      help="Specify whether to download images  or not")
+    parser.add_option("-n", "--name", dest="name", help="Name of model")
+    options, args = parser.parse_args()
+    if options.scrape is None and options.download is None:
+        print "Either scrape or download flag must be present"
+        exit(1)
+    if options.name is None:
+        print "Name must be present"
+        exit(2)
+    return options.scrape, options.download, options.name
+
+
 def get_file_path(model_name):
-    file_name = "_".join(model_name.strip().split())
+    file_name = replace_space_with_underscore(model_name.title())
     return os.path.join(os.path.join(OUTPUT_DIRECTORY, file_name), file_name)
 
 
 def get_file_content_as_json(file_path):
-    with open(file_path) as f:
-        file_content = json.load(f)
+    with open(file_path) as file_handle:
+        file_content = json.load(file_handle)
     return file_content
 
 
@@ -33,22 +76,25 @@ def get_image_urls(model_data):
 
 
 def scrape_model(model_name):
-    os.system(SCRAPE_COMMAND + '"' + model_name + '"')
+    scrape_model_command(model_name)
 
 
 def download_images(model_name, image_urls):
-    current_directory = os.getcwd()
-    file_name = "_".join(model_name.strip().split())
-    os.chdir(os.path.join(OUTPUT_DIRECTORY, file_name))
+    file_name = replace_space_with_underscore(model_name.title())
+    download_directory = os.path.join(OUTPUT_DIRECTORY, file_name)
     for image_url in image_urls:
         if image_url is not None:
-            os.system(DOWNLOAD_COMMAND + '"' + image_url + '"')
-    os.chdir(current_directory)
+            download_image(image_url, download_directory)
 
 
 if __name__ == '__main__':
-    name = str(input()).strip()
-    scrape_model(name)
-    file = get_file_path(name)
-    model_json = get_file_content_as_json(file)
-    download_images(name, get_image_urls(model_json))
+    scrape, download, name = parse_command_line_arguments()
+    if scrape:
+        print "Scraping  %s", name
+        scrape_model(name)
+    file_path = get_file_path(name)
+    model_json = get_file_content_as_json(file_path)
+    if download:
+        print "Download %s", name
+        download_images(name, get_image_urls(model_json))
+
